e44e8526c732933c2ca21802a4b6e2f3e135c96c
Owais Qureshi <qureshiowais004@gmail.com>
2025-12-23 20:17:04 +0530

    Create day1-printing.py

---
+++ day1-printing.py
@@ -0,0 +1,24 @@
+# --- Practice: Day 1 ---
+# Topic: Basic Printing and Math
+
+print("Hello World!")
+print(5)
+print("Bye")
+print(17 * 3)
+
+print("-" * 20) # This prints a divider line
+
+# --- Quick Quiz ---
+# Goal: Write a program to print a poem of your choice.
+# Poem: "Nothing Gold Can Stay" by Robert Frost
+
+print("""Nature's first green is gold,
+Her hardest hue to hold,
+Her early leaf's a flower;
+But only so an hour.
+
+Then leaf subsides to leaf,
+So Eden sank to grief,
+So dawn goes down to day,
+Nothing gold can stay.""")
+
